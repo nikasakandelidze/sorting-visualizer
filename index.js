0d47cd5ac36c58b55728af2e2aeb072c36f57257
nika <nikasakana@gmail.com>
2021-09-12 15:49:08 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,97 @@
+const elements = [20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]
+const visualDelay = 20;
+
+
+const elementsContainer = document.getElementById('elements-container');
+const insertionSortButton = document.getElementById('insertion_sort_button');
+const bubbleSortButton = document.getElementById('bubble_sort_button')
+const resetButton = document.getElementById('reset_button')
+
+function drawElements(elements){
+    let maxElement = Math.max(...elements);
+    elements.forEach( (element,idx) => {
+        let column = document.createElement('div');
+        column.className = 'column';
+        let heightInPixels = calculateHeightInPixels(element, maxElement);
+        column.style.height = `${heightInPixels}px`;
+        column.style.top = `${window.innerHeight - heightInPixels}px`
+        column.style.left = `${idx * 20}px`
+        elementsContainer.appendChild(column);
+    });
+}
+
+function calculateHeightInPixels(value, maxValue){
+    let innerHeight = window.innerHeight;
+    let result = (value * innerHeight) / maxValue;
+    return result;
+}
+
+function setupListeners(){
+    window.addEventListener('resize', event => {
+        elementsContainer.innerHTML='';
+        drawElements(elements);
+    });
+    insertionSortButton.addEventListener('click', event => {
+        insertionSort(elements);
+    });
+    bubbleSortButton.addEventListener('click', event => {
+        bubbleSort(elements);
+    });
+    resetButton.addEventListener('click', event => {
+        shuffleArray(elements);
+    });
+    window.dispatchEvent(new Event('resize'))
+}
+
+async function insertionSort(inputArr) {
+    let n = inputArr.length;
+        for (let i = 1; i < n; i++) {
+            let current = inputArr[i];
+            let j = i-1; 
+            while ((j > -1) && (current < inputArr[j])) {
+                inputArr[j+1] = inputArr[j];
+                j--;
+            }
+            inputArr[j+1] = current;
+            window.dispatchEvent(new Event('resize'))
+            await sleep(visualDelay);
+        }
+    return inputArr;
+}
+
+async function bubbleSort(inputArr){
+    let len = inputArr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (inputArr[j] > inputArr[j + 1]) {
+                let tmp = inputArr[j];
+                inputArr[j] = inputArr[j + 1];
+                inputArr[j + 1] = tmp;
+                window.dispatchEvent(new Event('resize'))
+                await sleep(visualDelay);
+            }
+        }
+    }
+    return inputArr;
+};
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        window.dispatchEvent(new Event('resize'));
+        await sleep(visualDelay);
+    }
+}
+
+setupListeners();
+
+
+
+
